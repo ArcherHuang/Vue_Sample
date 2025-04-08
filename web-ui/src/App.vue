a5bd30f1c7f9f7d24f19753831d4a839cf9f3c66
ArcherHuang <mmosconii@gmail.com>
2025-04-08 13:08:05 +0800

    init

---
+++ web-ui/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="full-screen center">
+    <button type="button" class="btn btn-outline-dark">Dark</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+.full-screen {
+  height: 100vh;
+  width: 100vw;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ web-ui/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    HelloWorld
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+    }
+  },
+  props: {
+    msg: String
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
